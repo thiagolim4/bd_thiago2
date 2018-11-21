65346981e28d879610b4efb0a4a89574fb540c00
Thiago Dong Chen <thiagodchen@gmail.com>
2018-11-21 15:15:09 -0200

    update xml files

---
+++ data/gera_xml.py
@@ -0,0 +1,14 @@
+filenames = ['PacientePossuiCaracteristica.xml', 
+                'Classificacao.xml', 
+                    'Paciente.xml', 
+                        'ClassificacaoTemSintoma.xml',
+                            'Caracteristica.xml',
+                                'Sintoma.xml']
+with open('column.xml', 'w') as outfile:
+    outfile.write('<?xml version="1.0" encoding="UTF-8"?>\n<root>\n')
+    for fname in filenames:
+        with open(fname) as infile:
+            next(infile)
+            for line in infile:
+                outfile.write(line)
+    outfile.write('\n<\\root>\n')
